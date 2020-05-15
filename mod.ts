0cba7b107322325f223efc7cc9fd3abffe85cbf8
Anand Chowdhary <mail@anandchowdhary.com>
2020-05-16 01:45:34 +0530

    :recycle: Rename export to qrcode

--- mod.ts
+++ mod.ts
@@ -1,32 +1,36 @@
-// @deno-types="./qrcode.d.ts"
-import { qrcode } from "./qrcode.js";
-export async function yaqrcode (text:string, options?:Options):Promise<QRCode> {
-    options = options || {};
-    const typeNumber: TypeNumber = options.typeNumber || 4;
-    const errorCorrectLevel: ErrorCorrectionLevel = options.errorCorrectLevel || 'M';
-    const size:number = options.size || 500;
+// @deno-types="./_qrcode.d.ts"
+import { qrcode as _qrcode } from "./qrcode.js";
 
-    let qr;
+export async function qrcode(
+  text: string,
+  options?: Options,
+): Promise<_QRCode> {
+  options = options || {};
+  const typeNumber: TypeNumber = options.typeNumber || 4;
+  const errorCorrectLevel: ErrorCorrectionLevel = options.errorCorrectLevel ||
+    "M";
+  const size: number = options.size || 500;
 
-    try {
-        qr = qrcode(typeNumber, errorCorrectLevel || 'M');
-        qr.addData(text);
-        qr.make();
-    } catch (e) {
-        if(typeNumber >= 40) {
-            throw new Error('Text too long to encode');
-        } else {
-            return yaqrcode(text, {
-                size: size,
-                errorCorrectLevel: errorCorrectLevel,
-                typeNumber: (typeNumber + 1 as TypeNumber)
-            });
-        }
+  let qr;
+
+  try {
+    qr = _qrcode(typeNumber, errorCorrectLevel || "M");
+    qr.addData(text);
+    qr.make();
+  } catch (e) {
+    if (typeNumber >= 40) {
+      throw new Error("Text too long to encode");
+    } else {
+      return qrcode(text, {
+        size: size,
+        errorCorrectLevel: errorCorrectLevel,
+        typeNumber: (typeNumber + 1 as TypeNumber),
+      });
     }
+  }
 
-    // calc cellsize and margin
-    const cellsize = Math.floor(size / qr.getModuleCount());
-    const margin = Math.floor((size - qr.getModuleCount() * cellsize) / 2);
+  const cellsize = Math.floor(size / qr.getModuleCount());
+  const margin = Math.floor((size - qr.getModuleCount() * cellsize) / 2);
 
-    return qr.createDataURL(cellsize, margin, size);
-};
+  return qr.createDataURL(cellsize, margin, size);
+}
