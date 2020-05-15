ca3bd549935bdd7747316878dcdb3a469aabe618
opliko <25460763+oplik0@users.noreply.github.com>
2020-05-15 21:38:00 +0200

    Add cli

--- cli.ts
+++ cli.ts
@@ -1,5 +1,4 @@
 import { yaqrcode } from "./mod.ts";
 
-for (let arg of Deno.args) {
-    console.log(arg, yaqrcode(arg));
-}
+console.log(`${Deno.args.join(" ")}:
+${await yaqrcode(Deno.args.join(" "))}`);
